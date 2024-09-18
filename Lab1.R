c8b0f0a2b714ef3b5d8afdf7f37e9297d409f313
Issac Liu <z5418189@ad.unsw.edu.au>
2024-09-18 11:29:27 +1000

    finished lab q3

--- Lab1.R
+++ Lab1.R
@@ -7,6 +7,10 @@ buffon=function(n, d=1, ell=1, make.plot=TRUE) {
   x=runif(n,0,d) # generate samples of x
   theta=runif(n,0,pi) # generate samples of theta
   
+  #### issue with experiment, need pi to generate samples of theta but we're
+  #### trying to estimate pi so it's a bit backward
+  
+  
   L=sin(theta)
   int=(ell*L)>x # determine samplesd x values with correspond to an intersection
   C=cumsum(int) # calculate counts of intersection for each value of n
@@ -22,5 +26,18 @@ buffon=function(n, d=1, ell=1, make.plot=TRUE) {
 buffon(2000)
 
 ###############
-# Q3 b)
+# Q3 b) in one note
 
+# Q3 c)
+
+out = rep(NA, 1000)
+n=2000
+ell.seq=seq(0.1, 1, length=20)
+sd.out=rep(NA, length(ell.seq))
+for (j in 1:length(ell.seq)) {
+  for (i in 1:1000) {
+    out[i]=buffon(n, ell=ell.seq[j], make.plot = F)
+    sd.out[j]=sd(out)
+  }
+}
+plot(ell.seq, sd.out, xlab = "l", ylab = "Estimate Standard Error", type="l")
