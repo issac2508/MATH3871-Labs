4c2edd2f9f227bec727069d6f25e0dbba09bb430
Issac Liu <z5418189@ad.unsw.edu.au>
2024-09-21 14:34:11 +1000

    finished up to q3

--- lab2-Issac.R
+++ lab2-Issac.R
@@ -24,6 +24,43 @@ inverse_func = function(x) {
   }
 }
 
-obs = inverse_func(runif(5000)) # do in array form
+obs = rep(NA, length=5000)
+samples = runif(5000)
 
-hist(obs, probability=T)
+for (i in 1:5000) {
+  obs[i] = inverse_func(samples[i])
+}
+
+hist(obs, probability=T, breaks = 40, xlim=c(-4,4), ylim=c(0,1))
+# breaks changes the number of bins
+# xlim and ylim change the scale of the graph
+
+xvals=seq(-5,5,length=100)
+lines(xvals, exp(-sign(xvals)*2*xvals), col="purple")
+
+###########
+# Q3)
+gen_samples = function(num, lambda) {
+  return(-log(1-runif(num))/lambda)
+}
+
+lambda = 1 # choose random?
+
+par(mfrow=c(3,3), oma = c(0, 0, 2, 0))
+
+nums=c(10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000)
+xvals=seq(0,10,length=100)
+
+for (num in nums) {
+  hist(gen_samples(num, lambda), probability=T, breaks = 20, xlim=c(0,10),
+       ylim=c(0,1), main = paste("num samples = ", as.character(num)))
+  lines(xvals, lambda * exp(-lambda*xvals), col="purple")
+}
+
+mtext(paste("Lambda = ", as.character(lambda)), outer = TRUE)
+
+###########
+# Q4)
+
+
+
