4e322fe0c97e1548d3aa51a45ac729c93fbacb57
Issac Liu <z5418189@ad.unsw.edu.au>
2024-11-09 12:40:38 +1100

    finished up to q4

--- z5418189_MATH3871Assignment.R
+++ z5418189_MATH3871Assignment.R
@@ -10,9 +10,6 @@ set.seed(1234)
 #1. Do not delete or edit existing text in this template, only add to it. 
 #2. Ensure your student number is in the file name. 
 
-
-
-
 #-------------------------------------------------------------------------------
 
 
@@ -54,7 +51,8 @@ lpost.LR <- function(beta,X,y) {
   alpha = numeric(k)
   omega = 100 * diag(k)
   
-  loglikelihood = y%*%X%*%beta-log(1+exp(x%*%beta))
+  
+  loglikelihood = y%*%X%*%beta-as.matrix(sum(log(1+exp(X%*%beta))))
   # find logprior
   logprior = -0.5 * t(beta - alpha) %*% solve(omega) %*% (beta - alpha) -
     (k / 2) * log(2 * pi) - 0.5 * log(det(omega))
@@ -75,34 +73,34 @@ mhmcmc <- function(y, X, B, nsims, Sigma) {
   beta_star = matrix(0, nrow = nsims, ncol = k)
   acc = 0
   
-  beta = numeric(k)
+  beta = matrix(0, nrow=k, ncol=1)
   
-  for (t in 1:nsims) {
+  
+  for (i in 1:nsims) {
     prop_beta = mvrnorm(1, mu=beta, Sigma=Sigma)
-    
+
     log_post_current = lpost.LR(beta, X, y)
     log_post_proposed = lpost.LR(prop_beta, X, y)
     
-    accprob[t] = exp(log_post_proposed - log_post_current)
-    acceptance_threshold = min(1, accprob[t])
+    print(log_post_proposed)
+    print(log_post_current)
+
+    accprob[i] = exp(log_post_proposed - log_post_current)
+    acceptance_threshold = min(1, accprob[i])
     
-    if (runif(1) < acceptance_threshold) {
+    if (runif(1) <= acceptance_threshold) {
       beta = prop_beta  # Accept the proposed value
       acc = acc + 1     # Increment accepted count
     }
     
-    beta_mat[t, ] <- beta
-    beta_star[t, ] <- prop_beta
+    beta_mat[i, ] = beta
+    beta_star[i, ] = prop_beta
   }
-  
   
   #return variables 
   mhout = list("beta_mat"=beta_mat, "accprob"=accprob, "beta_star" = beta_star, "acc"=acc)
   return(mhout)
-
 }
-
-
 
 #-------------------------------------------------------------------------------
 #QUESTION 4. 
@@ -110,16 +108,25 @@ mhmcmc <- function(y, X, B, nsims, Sigma) {
 #Covariance for proposal: 
 Sigma = diag(c(1100, 0.0015, 0.04, 0.05, 0.0005, 0.4, 0.00001, 0.000001, 0.0001, 0.09, 0.03, 0.002)) #Do not modify  
 
-mhout1 = mhmcmc(y=wine$good, X=wine, B=mleest, nsims=10^5, Sigma=Sigma)
+X=cbind(1, as.matrix(subset(wine, select = -good)))
+y=t(as.matrix(wine$good))
+
+mhout1 = mhmcmc(y=y, X=X, B=mleest, nsims=10^5, Sigma=Sigma)
 
 #(a) produce trace plots:
 
-par(mfrow=c(2,2))
+num_coefficients = ncol(mhout1$beta_mat)
+names = c(list("intercept"), colnames(subset(wine, select = -good)))
 
+par(mfrow=c(4,3))
+for (i in 1:num_coefficients) {
+  plot(mhout1$beta_mat[, i], type = "l", main = paste("Trace Plot for", names[i]),
+       xlab = "Iteration", ylab = "Coefficient Value")
+}
 
 
 #(b) proportion of accepted moves:
-mh4acc = mhout1.accprob
+mh4acc = mhout1$acc/10^5
 
 
 #-------------------------------------------------------------------------------
